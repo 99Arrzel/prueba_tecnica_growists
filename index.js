6f3843d860752cfeb15f09ed5034cd682f827660
Andres Carrillo <af.carrillo@live.com>
2024-10-22 20:00:05 -0400

    Tarea 1

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 /**
  * Im going to supose tasks are just notes and this is like a todo list
+ * Just simple fs
  */
 const file = require("fs");
 //We check if file exists
 const exists = file.existsSync("./tasks.json");
 let taskList;
 if (exists) {
-  taskList = file.readFileSync("./tasks.json");
+  taskList = JSON.parse(file.readFileSync("./tasks.json"));
 }
 if (!taskList) {
   taskList = [];
@@ -17,16 +18,16 @@ function addTask(task) {
     taskList = [];
   }
   taskList.push(task);
-  file.writeFileSync("./tasks.json", JSON.stringify(taskList));
+  // file.writeFileSync("./tasks.json", JSON.stringify(taskList));
 }
 function deleteTask(idx) {
   taskList.splice(idx, 1);
-  file.writeFileSync("./tasks.json", JSON.stringify(taskList));
+  // file.writeFileSync("./tasks.json", JSON.stringify(taskList));
 }
 
 function completeTask(idx) {
   taskList[idx].completed = true;
-  file.writeFileSync("./tasks.json", JSON.stringify(taskList));
+  // file.writeFileSync("./tasks.json", JSON.stringify(taskList));
 }
 function listPendingTasks() {
   return taskList.filter((task) => !task.completed);
@@ -34,9 +35,13 @@ function listPendingTasks() {
 function saveTasks() {
   file.writeFileSync("./tasks.json", JSON.stringify(taskList));
 }
-//Read input
 let isRunning = true;
+let sessionIsSaved = true;
 do {
+  if (!sessionIsSaved) {
+    //IDK why the requirement is not auto save but ok
+    console.log("Session is not saved, please save tasks");
+  }
   console.log("Pending tasks: ", listPendingTasks().length);
   console.log("1. Add task");
   console.log("2. Delete task");
@@ -48,23 +53,38 @@ do {
   if (input === 1) {
     let task = prompt("Enter task: ");
     addTask({ task, completed: false });
+    sessionIsSaved = false;
   }
   if (input === 2) {
-    listPendingTasks();
+    console.table(listPendingTasks());
     let idx = parseInt(prompt("Enter index: "));
     deleteTask(idx);
+    sessionIsSaved = false;
   }
   if (input === 3) {
     let idx = parseInt(prompt("Enter index: "));
-    completeTask(idx);
+    //Safe check, just to be sure...
+    if (idx > taskList.length || idx < 0) {
+      console.log("Invalid index");
+    } else {
+      completeTask(idx);
+      sessionIsSaved = false;
+    }
   }
   if (input === 4) {
-    console.log(listPendingTasks());
+    console.table(listPendingTasks());
   }
   if (input === 5) {
+    saveTasks();
+    sessionIsSaved = true;
   }
   if (input === 6) {
+    if (!sessionIsSaved) {
+      let save = prompt("Do you want to save tasks? (y/n)");
+      if (save?.toLowerCase().startsWith("y")) {
+        saveTasks();
+      }
+    }
     isRunning = false;
   }
-  saveTasks();
 } while (isRunning);
